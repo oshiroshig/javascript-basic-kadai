e4502271e9a1a458fd46435d69797ac954eb6469
oshiroshig <58937295+oshiroshig@users.noreply.github.com>
2024-01-26 19:41:23 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,29 @@
+const holidays = [
+  "元日",
+  "成人の日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+];
+for (let i = 0; i < holidays.length; i++) {
+  console.log(holidays[i]);
+}
+
+/------------------
+
+const holidays2 = [
+  "元日",
+  "成人の日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+];
+let i2 = 0;
+while (i2 < holidays2.length) {
+  console.log(holidays2[i2]);
+  i2++;
+}
